4c55ff866941f827eacdfa040fed60e7906ecbe6
doodybrains <doodybrainz@gmail.com>
2019-07-31 16:04:53 -0400

    wip

--- src/templates/section.js
+++ src/templates/section.js
@@ -15,34 +15,37 @@ class SectionTemplate extends Component {
       <Layout>
         <h2 style={{marginBottom: '40px'}}>{title}</h2>
         <h3 style={{marginBottom: '40px', color: 'white', fontSize: '2.25em'}}>{metadata.description}</h3>
-        {children
-          .filter(item => item.__typename === 'ArenaBlock')
-          .map((item, index) => {
-            console.log(item);
-            if (item.image) {
-              return (
-                <div key={index} style={{padding: '0 20%', margin: '30px auto 30px auto'}}>
-                  <Img
-                    fluid={item.image.childImageSharp.fluid}
-                  />
-                </div>
-              )
-            } else if (item.content_html) {
-              let itemTitle;
-              if (item.title) itemTitle = item.title;
-              return (
-                <div key={index}>
-                  <h4 style={{padding: '0 2%', color: 'white', textAlign: 'center', marginBottom: '20px', fontSize: '1.75em'}}>{itemTitle}</h4>
-                  <div
-                    style={{padding: '0 2%', color: 'white', textAlign: 'center', marginBottom: '20px', fontSize: '1.25em'}}
-                    dangerouslySetInnerHTML={{ __html: item.content_html }}
-                  />
-                </div>
-              )
-            } else {
-              return null;
-            }
-          })}
+
+        <div className="grid">
+          {children
+            .filter(item => item.__typename === 'ArenaBlock')
+            .map((item, index) => {
+              console.log(item);
+              if (item.image) {
+                return (
+                  <div className="image" key={index}>
+                    <Img
+                      fluid={item.image.childImageSharp.fluid}
+                    />
+                  </div>
+                )
+              } else if (item.content_html) {
+                let itemTitle;
+                if (item.title) itemTitle = item.title;
+
+                return (
+                  <div className="text" key={index}>
+                    <h4>{itemTitle}</h4>
+                    <div
+                      dangerouslySetInnerHTML={{ __html: item.content_html }}
+                    />
+                  </div>
+                )
+              } else {
+                return null;
+              }
+            })}
+        </div>
       </Layout>
     )
   }
